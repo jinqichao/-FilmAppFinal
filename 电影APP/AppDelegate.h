3b629e20d2c81c81a755550691eb97263e204033
金颀超 <jinqichao@jinqichaodeMac-mini.local>
2015-09-14 09:16:13 +0800

    Initial Commit

---
+++ 电影APP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  电影APP
+//
+//  Created by 金颀超 on 15/9/14.
+//  Copyright (c) 2015年 金颀超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 电影APP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  电影APP
+//
+//  Created by 金颀超 on 15/9/14.
+//  Copyright (c) 2015年 金颀超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
